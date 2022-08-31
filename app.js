b2f60dffa6c534a5af87e4c427340879062b735c
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-08-31 12:00:56 -0600

    feat: add delete note functionality"

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ class App {
      this.handleformClick(event);
      this.selectNote(event);
      this.openModal(event);
+     this.deleteNote(event);
     });
 
     document.body.addEventListener('mouseover', event => {
@@ -105,6 +106,8 @@ class App {
   }
 
   openModal(event) {
+    if (event.target.matches('.toolbar-delete')) return;  
+
     if (event.target.closest('.note')) {
       this.$modal.classList.toggle('open-modal');
       this.$modalTitle.value = this.title;
@@ -171,6 +174,14 @@ class App {
     this.id = $selectedNote.dataset.id;
   }
 
+  deleteNote(event) {
+    event.stopPropagation();
+    if (!event.target.matches('.toolbar-delete')) return;
+    const id = event.target.dataset.id;
+    this.notes = this.notes.filter(note => note.id !== Number(id));
+    this.displayNotes();
+  }
+
   displayNotes() {
     const hasNotes = this.notes.length > 0
     this.$placeholder.style.display = hasNotes ? 'none' : 'flex ';
@@ -182,7 +193,7 @@ class App {
           <div class="toolbar-container">
             <div class="toolbar">
               <img class="toolbar-color" data-id="${note.id}" src="assets/images/palette.png">
-              <img class="toolbar-delete" src="assets/images/delete.png">
+              <img data-id="${note.id}" class="toolbar-delete" src="assets/images/delete.png">
             </div>
           </div>
         </div>
